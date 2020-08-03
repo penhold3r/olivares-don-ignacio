0632cf9e751b1170e969430b3e688243bd0ec98f
penholder <penhold3r@gmail.com>
2020-08-03 16:21:55 -0300

    new products layout

--- src/data/products.js
+++ src/data/products.js
@@ -1,30 +1,61 @@
-import castelmonte from '../images/products/castelmonte-500ml.jpg'
-import giglio from '../images/products/giglio_organico-500ml.jpg'
-import oroliva from '../images/products/oroliva-500ml.jpg'
+import castelmonte500 from '../images/products/castelmonte_500.png'
+import castelmonte250 from '../images/products/castelmonte_250.png'
+import giglio500 from '../images/products/giglio_500.png'
+import giglio250 from '../images/products/giglio_250.png'
+import oroliva500 from '../images/products/oroliva_500.png'
+import oroliva1000 from '../images/products/oroliva_1000.png'
+import oroliva5000 from '../images/products/oroliva_5000.png'
+import oroliva3000 from '../images/products/oroliva_3000.png'
 
 export default [
 	{
 		brand: 'Castelmonte',
 		variety: 'Virgen Extra',
-		image: castelmonte,
-		qty: '500ml, 250ml',
-		desc:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, ipsum labore cum fugiat quibusdam.',
+		bottles: [
+			{
+				image: castelmonte250,
+				cont: '250ml',
+			},
+			{
+				image: castelmonte500,
+				cont: '500ml',
+			},
+		],
 	},
 	{
 		brand: 'Giglio',
 		variety: 'Extra Virgen Organico',
-		image: giglio,
-		qty: '500ml, 250ml',
-		desc:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, ipsum labore cum fugiat quibusdam.',
+		bottles: [
+			{
+				image: giglio250,
+				cont: '250ml',
+			},
+			{
+				image: giglio500,
+				cont: '500ml',
+			},
+		],
 	},
 	{
 		brand: 'Oroliva',
 		variety: 'Virgen Extra',
-		image: oroliva,
-		qty: '500ml, 1000ml, 3000ml, 5000ml',
-		desc:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, ipsum labore cum fugiat quibusdam.',
+		bottles: [
+			{
+				image: oroliva500,
+				cont: '500ml',
+			},
+			{
+				image: oroliva1000,
+				cont: '1000ml',
+			},
+			{
+				image: oroliva3000,
+				cont: '3000ml',
+			},
+			{
+				image: oroliva5000,
+				cont: '5000ml',
+			},
+		],
 	},
 ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { Parallax } from 'react-parallax'
 import hero from '../images/header.jpg'
 import oliveShape from '../images/olive.gif'
 import oliveTree from '../images/products_bg.jpg'
+import prodsHeader from '../images/olivares_don_ignacio-productos.jpg'
 
 import Wrapper from '../components/Wrapper'
 
@@ -21,31 +22,31 @@ const IndexPage = ({ location }) => {
 			state.toScroll === 'productos' &&
 			window.scrollTo({
 				behavior: 'smooth',
-				top: document.querySelector('#productos').offsetTop - 100
+				top: document.querySelector('#productos').offsetTop - 100,
 			})
 	}, [location])
 
 	return (
 		<Wrapper>
-			<div className="page">
-				<section className="hero">
+			<div className='page'>
+				<section className='hero'>
 					<Parallax bgImage={hero} strength={400} style={{ height: '100%' }}>
-						<div className="tagline">
-							<p className="text">
+						<div className='tagline'>
+							<p className='text'>
 								Más de 50 años de trayectoria brindando productos de calidad.
 							</p>
 						</div>
 					</Parallax>
 				</section>
 
-				<section className="history-landing">
-					<div className="history-intro">
-						<h4 className="t">
+				<section className='history-landing'>
+					<div className='history-intro'>
+						<h4 className='t'>
 							Más de 50 años de trayectoria brindando productos de calidad.
 						</h4>
 					</div>
-					<div className="content" style={{ backgroundImage: `url(${oliveShape})` }}>
-						<p className="prods">
+					<div className='content' style={{ backgroundImage: `url(${oliveShape})` }}>
+						<p className='prods'>
 							Aceite de oliva &#8226; Aceitunas &#8226; Productos Gourmet
 						</p>
 						<p>
@@ -53,29 +54,34 @@ const IndexPage = ({ location }) => {
 							olivicultura y vitivinicultura de Mendoza. Actualmente llevamos adelante la
 							empresa los cinco integrantes de la familia: papá, mamá y tres hermanos.
 						</p>
-						<Link className="history-link" to="/historia">
+						<Link className='history-link' to='/historia'>
 							Descubra nuestra historia
 						</Link>
 					</div>
 				</section>
 
-				<section className="products" id="productos">
-					<h2 className="prods-title">Nuestros Productos</h2>
-					<div className="prods-list" style={{ backgroundImage: `url(${oliveTree})` }}>
-						<div className="prods-grid">
-							{products &&
-								products.map((prod, key) => (
-									<div key={key} className="prod-card">
-										<img src={prod.image} alt={`bottle of ${prod.name}`} />
-										<h4 className="prod-name">{prod.brand}</h4>
-										<p className="prod-variety">{prod.variety}</p>
-										<div className="prod-details">
-											<p className="prod-desc">{prod.desc}</p>
-											<p className="pack">{prod.qty}</p>
-										</div>
-									</div>
-								))}
+				<section className='products' id='productos'>
+					<h2 className='prods-title'>Nuestros Productos</h2>
+					<div className='prods-list' style={{ backgroundImage: `url(${oliveTree})` }}>
+						<div className='prods-header'>
+							<img src={prodsHeader} alt='' />
 						</div>
+						{products &&
+							products.map((prod, key) => (
+								<div className='prods-grid' key={key}>
+									{prod.bottles.map((bottle, key) => (
+										<div key={key} className='prod-card'>
+											<img src={bottle.image} alt={`bottle of ${prod.brand}`} />
+											<h4 className='prod-name'>{prod.brand}</h4>
+											<p className='prod-variety'>{prod.variety}</p>
+											<div className='prod-details'>
+												{/* <p className='prod-desc'>{prod.desc}</p> */}
+												<p className='pack'>{bottle.cont}</p>
+											</div>
+										</div>
+									))}
+								</div>
+							))}
 					</div>
 				</section>
 			</div>
